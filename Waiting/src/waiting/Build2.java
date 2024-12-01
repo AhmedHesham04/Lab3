f4ebfa6aa598955da5cb77cf4cc06fb081d599f5
AhmedHesham04 <ahmedhesham1969@gmail.com>
2024-12-01 16:45:00 +0200

    finish class build2

---
+++ Waiting/src/waiting/Build2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package waiting;
+
+/**
+ *
+ * @author ahmed
+ */
+public class Build2 {
+    private int levels;
+
+    public void setLevels(int levels) {
+        this.levels = levels;
+    }
+
+    public int getLevels() {
+        return levels;
+    }
+
+    public Build2(int levels) {
+        this.levels = levels;
+    }
+}
